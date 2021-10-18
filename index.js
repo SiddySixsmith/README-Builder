6fc9847f53192b6c5c717443bea88971305c0bd7
Siddy <dancore99@outlook.com>
2021-10-18 22:20:55 +0800

    added to functions for repsonse to to work from

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 const name = "";
-const utlis = require("./utils/");
+const utlis = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-inquirer
-    .prompt = [
+
+const questions = [
         {
             type: "input",
             message: "What is your project title",
@@ -46,15 +47,61 @@ inquirer
             message: "Why did you build this project",
             name: "build",
         },
+        {
+            type: "input",
+            message: "Link to live demo",
+            name: "demo",
+        },
+        {
+            type: "input",
+            message: "link to code",
+            name: "code",
+        },
+        {
+            type: "select",
+            message: "What licence would you like",
+            choice: ['Apache License 2.0','GNU General Public License v3.0', 'MIT License' ],
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "github",
+          },
+          {
+            type: "input",
+            message: "What is your email address?",
+            name: "email",
+          },
     ]
-.then(response) 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile()
+.then((response) => {
+    const fileName = `README.md`
 }
+)
+// TODO: Create a function to write README file
+function writeToFile(fileName, response) {
+    fs.writeFile(fileName, utlis, (err) =>
+    err ? console.log(err) : console.log("sucess"));
+};
+console.log(response);
+
+function promptUser() {
+    return inquirer.prompt(choices);
 
 // TODO: Create a function to initialize app
-function init() { }
+async function init() {
+    try {
+      const response = await promptUser();    
+   // Generate result based on responses
+      const readMeText = generateMarkdown(response);
+      // Write to DEMO-README.md
+      await writeFile("README.md", readMeText);
+    // Log message when file is generated
+      console.log("Successfully written responses to DEMO-README.md");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,9 +11,34 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(response) {
+  return `
+  # ${response.project}
 
+  ## Description
+  ${response.description}
+  
+  - what was your modivation?
+  ${response.modivation}
+  - Why did you build this project?
+  ${response.build}
+
+
+  ## Installation
+  ${response.install}
+
+  ## Usage
+  ${response.usage}
+
+  ## Credits 
+  ${response.credit}
+
+ ## License
+ ${response.license}
+
+ ## Live Demo and Code
+ ${response.demo}
+ ${response.code}
 `;
 }
 
