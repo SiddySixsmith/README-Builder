889876ff9b5a9624a47ba9d23de199de0c3023ae
Siddy <dancore99@outlook.com>
2021-10-20 11:46:56 +0800

    questions show and readme file is created

--- index.js
+++ index.js
@@ -1,107 +1,87 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require('fs');
-const name = "";
-const utlis = require("./utils/generateMarkdown");
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-
 const questions = [
-        {
-            type: "input",
-            message: "What is your project title",
-            name: "project",
-        },
-        {
-            type: "input",
-            message: "What is your URL",
-            name: "url",
-        },
-        {
-            type: "input",
-            message: "What is the instalation instructions",
-            name: "install",
-        },
-        {
-            type: "input",
-            message: "What is the usage instructions",
-            name: "usage",
-        },
-        {
-            type: "input",
-            message: "Enter credits for your project",
-            name: "credit",
-        },
-        {
-            type: "input",
-            message: "What is the description of your project",
-            name: "description",
-        },
-        {
-            type: "input",
-            message: "What is your modivation",
-            name: "modivation",
-        },
-        {
-            type: "input",
-            message: "Why did you build this project",
-            name: "build",
-        },
-        {
-            type: "input",
-            message: "Link to live demo",
-            name: "demo",
-        },
-        {
-            type: "input",
-            message: "link to code",
-            name: "code",
-        },
-        {
-            type: "select",
-            message: "What licence would you like",
-            choice: ['Apache License 2.0','GNU General Public License v3.0', 'MIT License' ],
-        },
-        {
-            type: "input",
-            message: "What is your GitHub username?",
-            name: "github",
-          },
-          {
-            type: "input",
-            message: "What is your email address?",
-            name: "email",
-          },
-    ]
-.then((response) => {
-    const fileName = `README.md`
-}
-)
-// TODO: Create a function to write README file
-function writeToFile(fileName, response) {
-    fs.writeFile(fileName, utlis, (err) =>
-    err ? console.log(err) : console.log("sucess"));
-};
-console.log(response);
-
-function promptUser() {
-    return inquirer.prompt(choices);
+    {
+        type: "input",
+        message: "What is your project title",
+        name: "project",
+    },
+    {
+        type: "input",
+        message: "What is the instalation instructions",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "What is the usage instructions",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "Enter credits for your project",
+        name: "credit",
+    },
+    {
+        type: "input",
+        message: "What is the description of your project",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What is your modivation",
+        name: "modivation",
+    },
+    {
+        type: "input",
+        message: "Why did you build this project",
+        name: "build",
+    },
+    {
+        type: "input",
+        message: "Link to live demo",
+        name: "demo",
+    },
+    {
+        type: "input",
+        message: "link to code",
+        name: "code",
+    },
+    {
+        type: "list",
+        message: "What licence would you like",
+        choices: [
+            'Apache License 2.0',
+            'GNU General Public License v3.0',
+            'MIT License',
+            "BSD T-Clause 'Simplified' License",
+            "Creative Commons Zero v1.0 Universal",
+        ],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+];
 
 // TODO: Create a function to initialize app
 async function init() {
-    try {
-      const response = await promptUser();    
-   // Generate result based on responses
-      const readMeText = generateMarkdown(response);
-      // Write to DEMO-README.md
-      await writeFile("README.md", readMeText);
-    // Log message when file is generated
-      console.log("Successfully written responses to DEMO-README.md");
-    } catch (err) {
-      console.log(err);
-    }
-  }
-}
-
+    await inquirer.prompt(questions)
+    .then ((response) => { 
+    const data = generateMarkdown(response);
+    console.log(data);
+    fs.writeFileSync("README.md", data);
+        console.log("Successfully written responses to README.md");
+  });
+} 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,17 +13,18 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(response) {
+function generateMarkdown(response)  {
+
   return `
   # ${response.project}
 
   ## Description
   ${response.description}
   
-  - what was your modivation?
-  ${response.modivation}
+  - What was your modivation?
+  ${response.modivation}\n
   - Why did you build this project?
-  ${response.build}
+  ${response.build}\n
 
 
   ## Installation
